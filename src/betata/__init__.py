e8d1544d990de60d06537f4f4dbe2d00306b7b6f
Atharv Joshi <atharvjoshi@ymail.com>
2025-10-22 10:27:31 -0400

    Install package as editable

---
+++ src/betata/__init__.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+rcparams = plt.rcParams
+
+rcparams["font.sans-serif"] = "Avenir"
+rcparams["font.family"] = "sans-serif"
+rcparams["font.size"] = 16
+rcparams["lines.linewidth"] = 2
+
+rcparams["axes.linewidth"] = 2.0
+rcparams["axes.titlesize"] = 16
+rcparams["axes.labelsize"] = 16
+
+rcparams["xtick.labelsize"] = 16
+rcparams["ytick.labelsize"] = 16 
+rcparams["xtick.major.size"] = 8
+rcparams["xtick.major.width"] = 2
+rcparams["xtick.minor.size"] = 4
+rcparams["xtick.minor.width"] = 1
+
+rcparams["ytick.major.size"] = 8
+rcparams["ytick.major.width"] = 2
+rcparams["ytick.minor.size"] = 4
+rcparams["ytick.minor.width"] = 1
+
