48a6234dde8e30816e53710bac74f43dc5f6ae08
Atharv Joshi <atharvjoshi@ymail.com>
2025-11-19 21:40:42 -0500

    Setup KI fraction analysis for films 1 and 2

---
+++ src/betata/resonator_studies/alpha_vs_pitch_vs_thickness.py
@@ -0,0 +1,43 @@
+""" """
+
+from collections import defaultdict
+from pathlib import Path
+
+import h5py
+import numpy as np
+
+from betata import plt
+
+def get_alpha(fr_meas, fr_geom):
+    """ """
+    return 1 - (fr_meas / fr_geom) ** 2
+
+if __name__ == "__main__":
+    """ """
+    
+    input_folder = Path(__file__).parents[3] / "out/resonator_studies"
+
+    data = defaultdict(dict)
+
+    for filepath in input_folder.iterdir():
+        if filepath.suffix not in [".h5", ".hdf5"]:
+            continue
+        with h5py.File(filepath) as file:
+            pitch = file.attrs["pitch"]
+            thickness = file.attrs["film_thickness"]
+            fr_geom = file.attrs["fr_geom"]
+            fr_meas = file[list(file.keys())[0]].attrs["fr"]
+            alpha = get_alpha(fr_meas, fr_geom)
+
+            data[thickness][pitch] = alpha
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+    for thickness, inner_dict in data.items():
+        pitches = np.array(list(inner_dict.keys())) * 1e6
+        alphas = np.array(list(inner_dict.values()))
+        ax.scatter(pitches, alphas, label=f"{round(thickness * 1e9)}nm")
+    ax.set(xlabel="Pitch (um)", ylabel=r"$\alpha$")
+    ax.set_xticks([2, 4, 6, 8, 10, 12, 14, 16])
+    ax.legend()
+    fig.tight_layout()
+    plt.show()
--- src/betata/resonator_studies/resonator.py
+++ src/betata/resonator_studies/resonator.py
@@ -1,9 +1,71 @@
 """ """
 
 from dataclasses import dataclass
+from pathlib import Path
+
+import h5py
+import pandas as pd
+
+DATA_FOLDER = Path(__file__).parents[3] / "data/resonator_studies"
+SPR_SIM_FILEPATH = DATA_FOLDER / "spr_sim.csv"
 
 @dataclass
 class Resonator:
     """ """
 
     name: str
+    type: str
+    design_name: str
+    cooldown_name: str
+    film_thickness: float
+
+    # for cpw, pitch is the distance between center conductor and ground plane
+    # for le, pitch is the distance between capacitor pads
+    pitch: float
+
+    # for cpw, length and width are those of the center conductor
+    # for le, length and width are those of the inductor
+    length: float = None
+    width: float = None
+
+    # simulated resonance frequency and inductance assuming only geometric inductance
+    fr_geom: float = None
+    l_geom: float = None
+
+    # simulated surface participation ratios
+    p_ms: float = None # metal-substrate
+    p_ma: float = None # metal-air
+    p_sa: float = None # substrate-air
+    p_sub: float = None # substrate
+
+    # total attenuation in the input measurement chain
+    line_attenuation: float = None
+
+def save_resonator(resonator: Resonator, filepath: Path):
+    """ """
+    with h5py.File(filepath, "a") as file:
+        for key, value in resonator.__dict__.items():
+            file.attrs[key] = value
+
+def add_spr_metadata(resonator: Resonator):
+    """ """
+    df = pd.read_csv(SPR_SIM_FILEPATH)
+    pitch_um = round(resonator.pitch * 1e6)
+    metadata_row = df.loc[df["pitch (um)"] == pitch_um]
+    resonator.width, = metadata_row["width (um)"] * 1e-6
+    resonator.p_ms, = metadata_row["p_ms"]
+    resonator.p_ma, = metadata_row["p_ma"]
+    resonator.p_sa, = metadata_row["p_sa"]
+    resonator.p_sub, = metadata_row["p_sub"]
+    return resonator
+
+def add_inductance_metadata(resonator: Resonator, ):
+    """ """
+    film_id = resonator.name.split("_")[1]
+    inductance_sim_filepath = DATA_FOLDER / f"{film_id}_inductance_sim.csv"
+    df = pd.read_csv(inductance_sim_filepath)
+    pitch_um = round(resonator.pitch * 1e6)
+    metadata_row = df.loc[(df["pitch (um)"] == pitch_um) & (df["l_s (pH/sq)"] == 0)]
+    resonator.fr_geom, = metadata_row["fr_geom (GHz)"] * 1e9
+    resonator.l_geom, = metadata_row["l (nH)"] * 1e-9
+    return resonator
