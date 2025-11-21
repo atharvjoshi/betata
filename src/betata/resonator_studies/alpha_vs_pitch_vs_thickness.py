7cceebb01452f3a6df5730394d458d778d38d4f0
Atharv Joshi <atharvjoshi@ymail.com>
2025-11-20 22:02:32 -0500

    Include resonators from three more films, enough to generate part of fig 2

--- src/betata/resonator_studies/alpha_vs_pitch_vs_thickness.py
+++ src/betata/resonator_studies/alpha_vs_pitch_vs_thickness.py
@@ -36,14 +36,17 @@ if __name__ == "__main__":
             alpha = get_alpha(fr_meas, fr_geom)
 
             data[thickness][pitch] = alpha
+    
+    sorted_data = dict(sorted(data.items()))
 
-    fig, ax = plt.subplots(figsize=(8, 6))
-    for thickness, inner_dict in data.items():
+    fig, ax = plt.subplots(figsize=(12, 8))
+    for thickness, inner_dict in sorted_data.items():
         pitches = np.array(list(inner_dict.keys())) * 1e6
         alphas = np.array(list(inner_dict.values()))
         ax.scatter(pitches, alphas, label=f"{round(thickness * 1e9)}nm")
-    ax.set(xlabel="Pitch (um)", ylabel=r"$\alpha$")
+    ax.set(xlabel=r"CPW gap ($\mu$m)", ylabel=r"KI fraction $\alpha$")
     ax.set_xticks([2, 4, 6, 8, 10, 12, 14, 16])
-    ax.legend()
+    ax.set_yticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+    ax.legend(bbox_to_anchor=(1.05, 1))
     fig.tight_layout()
     plt.show()
